e9462452212616cac75d51d40d6a9bda7d69cd78
Douglas Moura <douglas.ademoura@gmail.com>
2023-08-28 10:13:36 -0300

    feat: cria arquivos para implantar o site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://douglasdemoura.github.io',
   integrations: [
     react(),
     tailwind({
